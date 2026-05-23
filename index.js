ce2204043de765838c9bd2bbe139da3243fc6d94
Soyer1310 <soyer131313@gmail.com>
2026-05-23 21:30:13 +0300

    add getTaskTitles and formatHighPriorityTasks functions

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-allTheTasks = [
+const tasks = [
   { id: 0, title: 'wake up', completed: true, priority: 'high' },
   { id: 1, title: 'go outside for a run', completed: false, priority: 'medium' },
   { id: 2, title: 'take a shower', completed: false, priority: 'high' },
   { id: 3, title: 'feed the cat', completed: false, priority: 'high' },
   { id: 4, title: 'make breakfast', completed: false, priority: 'medium' },
   { id: 5, title: 'eat breakfast', completed: false, priority: 'medium' },
-  { id: 6, title: 'watch youtuube', completed: false, priority: 'low' },
+  { id: 6, title: 'watch YouTube', completed: false, priority: 'low' },
 ];
 
 const getHighPriorityTasks = tasks => tasks.filter(task => task.priority === 'high' && !task.completed);
 
-console.log(getHighPriorityTasks(allTheTasks));
+const getTaskTitles = tasks => tasks.map(task => task.title);
+
+
+const formatHighPriorityTasks = tasks => tasks
+  .filter(task => task.priority === 'high')
+  .map(({ id, title, completed }) => ({
+     id, 
+     statusText: completed ? `ЗАВЕРШЕНО: ${title}` : `В РАБОТЕ: ${title}`
+  }));
+
+console.log(getTaskTitles(tasks));
+console.log(formatHighPriorityTasks(tasks));
