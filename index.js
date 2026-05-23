fe73cbc03283d3fe891ea9c75d80c3637203de7b
Soyer1310 <soyer131313@gmail.com>
2026-05-23 18:53:04 +0300

    add getHighPriorityTasks func

---
+++ index.js
@@ -0,0 +1,15 @@
+allTheTasks = [
+  { id: 0, title: 'wake up', completed: true, priority: 'high' },
+  { id: 1, title: 'go outside for a run', completed: false, priority: 'medium' },
+  { id: 2, title: 'take a shower', completed: false, priority: 'high' },
+  { id: 3, title: 'feed the cat', completed: false, priority: 'high' },
+  { id: 4, title: 'make breakfast', completed: false, priority: 'medium' },
+  { id: 5, title: 'eat breakfast', completed: false, priority: 'medium' },
+  { id: 6, title: 'watch youtuube', completed: false, priority: 'low' },
+];
+
+const getHighPriorityTasks = tasks => tasks.filter(task => task.priority === 'high');
+
+const highPriotityTasks = getHighPriorityTasks(allTheTasks);
+
+console.log(highPriotityTasks);
